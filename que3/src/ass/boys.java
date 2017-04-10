98f3ee2af3c60ede524f45a67d08e4d4d5fa30e0
Shuchi0225 <rit2015073@iiita.ac.in>
2017-04-10 08:56:48 +0530

    Add files via commit
    Ques 3

---
+++ que3/src/ass/boys.java
@@ -0,0 +1,36 @@
+
+package ass;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Shuchi
+ */
+
+/**
+ * abstract class containing general attributes of boys
+ * 
+ */  
+abstract public class boys {
+    /**
+     *Boys class containing :
+     * name {String} unique name of each boy
+     * attractive
+     * attraction_req minimum attractive_requirement of their girlfriends  
+     * gf_budget his budget for giving gifts to his gf
+     * relation_status current relationship status : single or committed
+     *  happiness happiness of each boy as per his type
+     *  intelligence intelligence level
+    */
+    public String name;                 
+    public int id=0;
+    public int attractiveness;
+    public int gf_budget;
+    public int intelligence; 
+    public int attration_req;
+    public String relation_status; 
+    public int happiness;
+    public int com_to_gid;
+    public abstract void happ();
+}
